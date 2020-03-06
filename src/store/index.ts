b009f5715c506ee8dac1a75762cf51adf69289f5
lavn <790883381@qq.com>
2020-03-06 18:20:42 +0800

    【song】swiper

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import home from './home/index'
+// import home from './home/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    home
+    // home
   }
 })
--- src/views/song/index.vue
+++ src/views/song/index.vue
@@ -2,11 +2,11 @@
   <div class="song">
     <div class="title"> 
       <span 
-        @click="titleIndex = 1"
-        :class="{'currentTitle': titleIndex === 1}">歌曲</span> | 
+        @click="switchPage(0)"
+        :class="{'currentTitle': currentIndex === 0}">歌曲</span> | 
       <span 
-        @click="titleIndex = 2"
-        :class="{'currentTitle': titleIndex === 2}">歌词</span> 
+        @click="switchPage(1)"
+        :class="{'currentTitle': currentIndex === 1}">歌词</span> 
     </div>
     <div class="content">
       <div class="container" 
@@ -15,7 +15,9 @@
         @touchmove="moveBox"
         @touchend="moveEnd">
         <div class="left">
-
+          <div class="img-box">
+            <img src="../../assets/image/2.jpg" alt="">
+          </div>
         </div>
 
         <div class="right">
@@ -33,7 +35,6 @@ import {Component, Prop, Vue, Watch} from 'vue-property-decorator'
 @Component
 export default class Song extends Vue{
   // data
-  titleIndex: number = 1
   start_X: number = 0
   currentIndex: number = 0
 
@@ -49,10 +50,33 @@ export default class Song extends Vue{
 
 
   /**
+   * 点击切换
+   * @params {number} page 点击页数
+   */
+  switchPage(page: number): void {
+    if (page === 0 && this.currentIndex !== 0) {
+      let el: any = this.$refs.swiper
+      const client_W: number = window.innerWidth
+      el.style.transform = `translateX(0px)`
+      el.style.transition = '300ms'
+      this.currentIndex = 0
+    }
+
+    if (page === 1 && this.currentIndex !== 1) {
+      let el: any = this.$refs.swiper
+      const client_W: number = window.innerWidth
+      el.style.transform = `translateX(${-client_W}px)`
+      el.style.transition = '300ms'
+      this.currentIndex = 1
+    }
+  }
+
+
+  /**
    * 获取初始坐标
    */
   moveStart(e: any): void {
-    console.log('test:>start', e)
+    console.log('test:>start')
     this.start_X  = e.targetTouches[0].pageX
   }
 
@@ -62,22 +86,25 @@ export default class Song extends Vue{
    */
   moveBox(e: any): void {
     const { start_X, currentIndex } = this
+
     let el: any = this.$refs.swiper
     const move_X: number = e.targetTouches[0].pageX
     const move_W: number = move_X  - start_X
-
-    // console.log('test:>move_W', move_W)
+    const client_W: number = e.targetTouches[0].target.clientWidth
     
     if (move_W > 0) {
       if (currentIndex === 0) {
         let move_r = move_W * 0.5
         el.style.transform = `translateX(${move_r}px)`
       } else {
-
+        el.style.transform = `translateX(${move_W}px)`
       }
     } else {
       if (currentIndex === 0) {
         el.style.transform = `translateX(${move_W}px)`
+      } else {
+        let move_r = move_W * 0.5 - client_W
+        el.style.transform = `translateX(${move_r}px)`
       }
     }
   }
@@ -87,28 +114,55 @@ export default class Song extends Vue{
    * 移动结束
    */
   moveEnd(e: any): void {
-    console.log('test:>end', e)
     let { start_X, currentIndex } = this
     let el: any = this.$refs.swiper
     const move_X: number = e.changedTouches[0].pageX
     const move_W: number = move_X  - start_X
     const client_W: number = e.changedTouches[0].target.clientWidth
 
+    if (move_W === 0) {
+      return 
+    }
+
     if (move_W > 0) {
+
       if (currentIndex === 0) {
         el.style.transform = `translateX(0px)`
         el.style.transition = '500ms'
-        this.start_X = 0
+      } else {
+
+        if (move_W >= client_W / 2) {
+          el.style.transform = `translateX(0px)`
+          el.style.transition = '500ms'
+          this.currentIndex = 0
+        } else {
+          el.style.transform = `translateX(${-client_W}px)`
+          el.style.transition = '500ms'
+        }
       }
+
+      this.start_X = 0
+
     } else {
+
       if (currentIndex === 0) {
         el.style.transform = `translateX(${-client_W}px)`
         el.style.transition = '500ms'
-        this.start_X = 0
         this.currentIndex = 1
       } else {
+        if (-move_W >= client_W / 2) {
+          el.style.transform = `translateX(${-client_W}px)`
+          el.style.transition = '500ms'
+          this.currentIndex = 1
+        } else {
+          el.style.transform = `translateX(0px)`
+          el.style.transition = '500ms'
+        }
         
       }
+
+      this.start_X = 0
+
     }
   }
   
@@ -140,8 +194,20 @@ export default class Song extends Vue{
       .left {
         width: 100vw;
         height: 100%;
-        background: red;
         float: left;
+        .img-box {
+          width: 80vw;
+          height: 80vw;
+          vertical-align: middle;
+          margin: 0 auto;
+          border-radius: 20px;
+          overflow: hidden;
+          img {
+            width: 80vw;
+            height: 80vw;
+            border-radius: 10px;
+          }
+        }
       }
       .right {
         width: 100vw;
